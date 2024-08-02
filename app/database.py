d8f1a33f5906aff84a62743114174680c9f950f1
Mohsin Idrees <mohsinidrees2817@gmail.com>
2024-08-03 00:19:54 +0500

    commited

--- app/database.py
+++ app/database.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+# DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "DRIVER={ODBC Driver 18 for SQL Server};SERVER=mls-data.database.windows.net;PORT=1433;DATABASE=MLS_data;UID=aigentymvp;PWD=ghpdGypWzGNGRh1AxKEA;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
 def get_db_connection():
     return pyodbc.connect(DATABASE_URL)
